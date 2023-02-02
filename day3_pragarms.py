fbb69bef5b27aa5e136501aa183352160e3ad3e4
NagallaManikanta <113751642+NagallaManikanta@users.noreply.github.com>
2023-02-02 09:29:36 +0530

    Add files via upload

---
+++ day3_pragarms.py
@@ -0,0 +1,11 @@
+number=[elements for elements in "good afternoon"]
+print(number)
+l=["mani","vijay","hari","kana","chinna","goooo"]
+for i in l:
+    if "a" in i:
+        print(i)
+l1=[2**x for x in range(1,20) if x%2==0]
+print(l1)
+l2=[1,2,3,4,5,6,7,8,9,]
+l3=[b for b in l2 if b<3]
+print(l3)
---
+++ day3_programs.py (2).py
@@ -0,0 +1,14 @@
+'''t=(1,2,3,3,4,4,5,6,7,8,9,8,76,54,32,3,4,5,6,7,8,7654)
+print(t.count(3))
+print(t.index(7))
+print(len(t))
+#dictionary
+d={1:"one",2:"two"}
+print(d.items())
+print(d.keys())'''
+l=[1,2,3,4,56]
+d=dict.fromkeys(l)
+d.pop(3)
+d.item(1)
+print(d)
+
